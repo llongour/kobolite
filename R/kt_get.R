aec4b898fe7185fc97bdd92ac3e0ce1238ca9dbb
lucas.longour_ird.fr <lucas.longour@protonmail.ch>
2023-02-15 11:52:39 +0700

    init repo

---
+++ R/kt_get.R
@@ -0,0 +1,54 @@
+#' Get list of projects
+#'
+#' @param endpoint api endpoint url
+#' @param username username
+#' @param password password
+#'
+#' @importFrom httr GET authenticate progress content
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ktb_projects_list(endpoint = "https://kc.humanitarianresponse.info/api/v1/",
+#'                   username = Sys.getenv("KTB_USER"),
+#'                   password = Sys.getenv("KTB_PWD"))
+#' }
+ktb_projects_list <- function(endpoint, username, password){
+  GET(url = paste0(endpoint, "data.csv"),
+      authenticate(user = username,
+                   password = password),
+      progress()
+  ) |>
+    content(type = "text/csv", encoding = "UTF-8")
+}
+
+#' Get data of project
+#'
+#' @param endpoint api endpoint url
+#' @param project_id project id
+#' @param username username
+#' @param password password
+#'
+#' @importFrom httr GET authenticate progress content
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ktb_project_data(endpoint = "https://kc.humanitarianresponse.info/api/v1/",
+#'                  project_id = 1,
+#'                  username = Sys.getenv("KTB_USER"),
+#'                  password = Sys.getenv("KTB_PWD")
+#' )
+#' }
+ktb_project_data <- function(endpoint, project_id, username, password){
+  GET(url = paste0(endpoint, "data/", project_id, ".csv"),
+      authenticate(user = username,
+                   password = password),
+      progress()
+  ) |>
+    content(type = "text/csv", encoding = "UTF-8")
+}
